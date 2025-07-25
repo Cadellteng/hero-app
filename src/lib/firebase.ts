2a80338b0404c1f712c25229a5938ac4300e75be
Kayderl <46107573+Cadellteng@users.noreply.github.com>
2025-07-25 16:28:01 +0800

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,6 +8,7 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// API Key deactivated
 const firebaseConfig = {
   apiKey: "AIzaSyAXp__cEoGICwwjpTxEs6NSVVEVYSXNxfw",
   authDomain: "hero-app-98294.firebaseapp.com",
